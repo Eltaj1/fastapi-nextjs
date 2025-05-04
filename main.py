aca12c4ef29fbbab66fcc58f600820a34f39e78d
Eltaj <sarizade.eltaj@gmail.com>
2025-05-04 18:52:36 +0200

    Added backend using FastApi

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to FastAPI!"}
+
+@app.get("/hello")
+async def hello():
+    return "Hello, World!"
